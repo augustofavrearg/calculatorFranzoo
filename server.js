8d3d6b057129be76834948e6981eb443b3bfa1bb
Augusto Ramon Favre <augustofavrearg@gmail.com>
2024-07-15 17:21:41 -0300

    feat: cambio estructura para deploy

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos
-app.use(express.static(path.join(__dirname, '/')));
+// Servir archivos estáticos desde el directorio 'public'
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta raíz para asegurarse de que no se obtiene "Cannot GET /"
+// Ruta raíz para servir el archivo HTML
 app.get('/', (req, res) => {
-    res.send('¡Hola, Mundo! Servidor funcionando correctamente.');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Ruta para scraping
